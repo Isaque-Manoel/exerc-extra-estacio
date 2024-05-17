d18480d8e6281e9b7a280d9af278b44d2816030d
Isaque Manoel <169088062+Isaque-Manoel@users.noreply.github.com>
2024-05-17 19:25:10 -0300

    tentativa1

---
+++ calculadora-simples/Js/scripts.js
@@ -0,0 +1,45 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }
+
+  // add digit to calculator screen
+  addDigit(digit) {
+    console.log(digit);
+    // Check if number already has a dot
+    if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+      return;
+    }
+
+    this.currentOperation = digit;
+    this.updateScreen();
+  }
+
+
+    //Change values of the calculator screen
+    updateScreen() {
+        this.currentOperationText.innerText += this.currentOperation
+    }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const value = e.target.innerText;
+
+    if (+value >= 0 || value === ".") {
+      console.log(value);
+      calc.addDigit(value);
+    } else {
+      calc.processOperation(value);
+    }
+
+    })
+})
